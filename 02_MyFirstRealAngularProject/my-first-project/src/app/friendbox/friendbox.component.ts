06c89de6bacdbf26a6242cf260b4e4cb78aa9c51
Svendolin <sven0815@gmx.ch>
2023-07-13 01:06:47 +0200

    Updated Some Sections

--- 02_MyFirstRealAngularProject/my-first-project/src/app/friendbox/friendbox.component.ts
+++ 02_MyFirstRealAngularProject/my-first-project/src/app/friendbox/friendbox.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendboxComponent implements OnInit {
 
+  names = [
+    'Lord Schnee',
+    'Mr. Cookie Dough',
+    'Zac Brown',
+    'Banshee'
+  ];
+
+  texts = [
+    'Vergeben',
+    'Hungrig',
+    '2,5 Jahre alt',
+    'Happy'
+  ];
+
+  images = [
+    'assets/img/hamster/5.jpg',
+    'assets/img/hamster/6.jpg',
+    'assets/img/hamster/7.jpg',
+    'assets/img/hamster/8.jpg'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
